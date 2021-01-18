bbaed1fe870629342922b6bfb46645c3c38a47a5
kevthedev007 <63928159+kevthedev007@users.noreply.github.com>
2021-01-19 00:31:37 +0100

    new files

--- App.js
+++ App.js
@@ -1,2 +1,37 @@
 const express = require('express');
+const path = require('path');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+const dotenv = require('dotenv');
+dotenv.config();
+
+//custom modules
+// const routes = require('./routes/router.js');
+
+//express app
+const app = express();
+
+//view engine systems
+app.set('views', path.join(__dirname + 'views'))
+app.set('view engine', 'pug');
+
+//adding middlewares
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(session({
+    secret: "kevthedev",
+    resave: false,
+    saveUninitilaized: false,
+}))
+app.use(flash())
+
+
+//adding routes
+// app.use('/', routes)
+
+app.listen(process.env.PORT, () => {
+    console.log(`app started at ${process.env.PORT}`)
+})
